0cc6374f16d46aac9d517edcd280f2c8995ea7de
Michael Chapman <woppin@gmail.com>
2012-04-10 21:30:55 +1000

    I say bail

--- gravbot/world.py
+++ gravbot/world.py
@@ -9,12 +9,13 @@ from player import Player
 from panda3d.bullet import BulletWorld
 from panda3d.bullet import BulletPlaneShape
 from panda3d.bullet import BulletBoxShape
+from panda3d.bullet import BulletConvexHullShape 
 from panda3d.bullet import BulletRigidBodyNode
 
 from math import hypot 
 import utilities
 
-worldsize = Point2(100,100)
+worldsize = Point2(10,10)
 
 class World():
     CULLDISTANCE = 10
@@ -41,6 +42,25 @@ class World():
 
 	self.culls = 0
 
+	self.chtest = BulletConvexHullShape()
+	h = 0.5
+
+	self.chtest.addArray([
+	    Point3(h,h,h),
+	    Point3(-h,h,h),
+	    Point3(h,-h,h),
+	    Point3(-h,-h,h),
+	    Point3(h,h,-h),
+	    Point3(-h,h,-h),
+	    Point3(h,-h,-h),
+	    Point3(-h,-h,-h),
+	    ])
+	self.chbn = BulletRigidBodyNode()    
+        self.chbn.addShape(self.chtest)
+	self.chnp = render.attachNewNode(self.chbn)
+	self.chnp.setPos(0,20,30)
+	self.bw.attachRigidBody(self.chbn)
+
     def update(self, timer):
         dt = globalClock.getDt()
         self.bw.doPhysics(dt)
@@ -61,29 +81,74 @@ class World():
         for i in range(-10, 10):
             self.entities.append(Rail(self, i * 10, 0))
     
-        pt = list()
+        self.pt = list()
 
         for i in range(0, int(worldsize.x)):
-            pt.append(list())
+            self.pt.append(list())
             for j in range(0, int(worldsize.y)):
                 if self.perlin.noise(i,j) > 0:
-                    pt[i].append(1)
+                    self.pt[i].append(1)
                 else:
-                    pt[i].append(0)  
+                    self.pt[i].append(0)  
 
-        pt[0][0] = 0	
-        pt[1][0] = 0	
-        pt[0][1] = 0	
-        pt[1][1] = 0	
-        pt[2][2] = 0
-        pt[2][1] = 0
-        pt[1][2] = 0
-        pt[2][2] = 0 
+        #pt[0][0] = 0	
+        #pt[1][0] = 0	
+        #pt[0][1] = 0	
+        #pt[1][1] = 0	
+        #pt[2][2] = 0
+        #pt[2][1] = 0
+        #pt[1][2] = 0
+        #pt[2][2] = 0 
 
         for i in range(2, int(worldsize.x)):
             for j in range(2, int(worldsize.y)):
-                if pt[i][j] == 1:
-                    self.entities.append(Wall(self, Point2(i,j)))
+                if self.pt[i][j] == 1:
+		    pass
+                    #self.entities.append(Wall(self, Point2(i,j)))
+
+        self.printpt()
+	self.wgs = list()
+	self.searchForWalls(self.pt)
+
+	for wallblock in self.wgs:
+	    print "block"
+	    for block in wallblock:
+	        print block
+                self.entities.append(Wall(self, Point2(block.x+5,block.y+5)))
+
+    def searchForWalls(self, pt):
+        for i in range(0, int(worldsize.x)):
+            for j in range(0, int(worldsize.y)):
+	        if self.pt[i][j] == 1:
+		  points = list()
+		  self.wgs.append(points)
+		  self.makeWallGroup(self.pt, Pix(i,j), points)
+
+    def makeWallGroup(self, pt, point, points):
+        i = point.x
+	j = point.y
+	if self.pt[i][j] == 1:
+	    self.pt[i][j] = 0
+	    points.append(Pix(i,j))
+	else: 
+	    return    
+        if i > 0:
+	    self.makeWallGroup(self.pt, Pix(i-1,j), points)
+        if i < worldsize.x-1:  
+	    self.makeWallGroup(self.pt, Pix(i+1,j), points)
+ 	if j < worldsize.y-1:
+	    self.makeWallGroup(self.pt, Pix(i,j+1), points)
+	if j > 0:
+	    self.makeWallGroup(self.pt, Pix(i,j-1), points)
+
+    def printpt(self):
+	astr = ""
+        for i in range(0, int(worldsize.x)):
+	    tstr = ""
+            for j in range(0, int(worldsize.y)):
+	        tstr = tstr +  str(self.pt[j][i]) + " "
+	    astr = tstr + '\n' + astr
+	print astr    
     
     def addEntity(self, entity):
         self.entities.append(entity)
@@ -103,27 +168,31 @@ def distance(p1, p2):
     return hypot(p1.x-p2.x, p1.y - p2.y) 
 
 class Wall(Entity):
-    def __init__(self, world, pos):
+    def __init__(self, world, pos, grouped = False):
         super(Wall, self).__init__()
         self.health = 100
 	self.world = world
 
-        shape = BulletBoxShape(Vec3(0.5,2.0,0.5))
-        self.bnode = BulletRigidBodyNode()
-        self.bnode.addShape(shape)
-        self.np = utilities.app.render.attachNewNode(self.bnode)
-        self.np.setPos(pos.x,20,pos.y)
-        #world.bw.attachRigidBody(self.bnode)
+	if not grouped:
+            shape = BulletBoxShape(Vec3(0.5,2.0,0.5))
+            self.bnode = BulletRigidBodyNode()
+            self.bnode.addShape(shape)
+            self.np = utilities.app.render.attachNewNode(self.bnode)
+            self.np.setPos(pos.x,20,pos.y)
+            #world.bw.attachRigidBody(self.bnode)
 
-        self.obj = utilities.loadObject("wall", depth = 0)
-        self.obj.reparentTo(self.np)
-        self.obj.setScale(1)
-	self.obj.hide()
+            self.obj = utilities.loadObject("wall", depth = 0)
+            self.obj.reparentTo(self.np)
+            self.obj.setScale(1)
+	    self.obj.hide()
 
-	#for storing state if we are far from the camera
-	self.inScope = False
-	self.hpr = Point3(0,0,0) 
-	self.pos = pos 
+	    #for storing state if we are far from the camera
+	    self.inScope = False
+	    self.hpr = Point3(0,0,0) 
+	    self.pos = pos 
+	else:
+	    return
+	    
 
     def update(self, timer):
 	d = distance(self.pos, self.world.player.location)
@@ -138,31 +207,69 @@ class Wall(Entity):
         if self.health < 0:
             self.obj.remove()
 
-    def addToNode(self, pos):
-        """
-        Need to make convex hulls or a heightmap
-        so we can have bigger numbers of active physics objects
-        """
-        return
+# Probably want to convert the list of points to a graph to support
+# cutting an object into two pieces
+class WallGroup(Entity):
+    def __init__(self, world, pos, wallpoints):
+        super(WallGroup, self).__init__()
 
-    def expandNode(self, pt):
-        for i in range(1, int(worldsize.x)-1):
-            for j in range(1, int(worldsize.y)-1):
-                if pt[i][j] == 1:
-                    self.entities.append(Wall(self, Point2(i,j)))
-                if pt[i-1][j-1] == 1 : 
-                    self.entities[-1].addToNode(Point2(i,j))
-                    pt[i][j] = -1
-                if pt[i-1][j] == 1 : 
-                    self.entities[-1].addToNode(Point2(i,j))
-                    pt[i][j] = -1
-                if pt[i-1][j+1] == 1 : 
-                    self.entities[-1].addToNode(Point2(i,j))
-                    pt[i][j] = -1
-                if pt[i][j-1] == 1 : 
-                    self.entities[-1].addToNode(Point2(i,j))
-                    pt[i][j] = -1
-                if pt[i][j+1] == 1 : 
-                    self.entities[-1].addToNode(Point2(i,j))
-                    pt[i][j] = -1
+	self.mass = len(wallpoints)
+	# TODO  want to narrow this down to minimum set
+	self.hullpoints = list()
 
+        # the starting wall has all vertices
+	# subsequent ones will just add another face
+	self.hullpoints.append(Point3(0.5, 0.5, 0.5))
+	self.hullpoints.append(Point3(0.5, 0.5, -0.5))
+	self.hullpoints.append(Point3(-0.5, 0.5, 0.5))
+	self.hullpoints.append(Point3(-0.5, 0.5, -0.5))
+
+	self.hullpoints.append(Point3(0.5, -0.5, 0.5))
+	self.hullpoints.append(Point3(0.5, -0.5, -0.5))
+	self.hullpoints.append(Point3(-0.5, -0.5, 0.5))
+	self.hullpoints.append(Point3(-0.5, -0.5, -0.5))
+	#self.walls.append(Wall())
+	self.walls = list()
+
+        self.prevwall = wallpoints[0]
+	self.prevwall.x -= pos.x
+	self.prevwall.y -= pos.z
+	for wall in wallpoints[1:]:
+	    # set the wall positions relative to the first node
+	    # this shouldn't really be a problem even if the first node disappears
+	    # though it does mean we need to be careful about getting the NodePath
+	    # position because it's essentially meaningless
+	    wall.x -= pos.x
+	    wall.y -= pos.z
+
+            xmod = 0
+	    ymod = 0
+	    # figure out which direction we're heading in
+
+	    self.hullpoints.append(wall.x, 0, wall.y)
+	    self.hullpoints.append(wall.x, 0, wall.y)
+	    self.hullpoints.append(wall.x, 0, wall.y)
+	    self.hullpoints.append(wall.x, 0, wall.y)
+
+	    self.prevwall = wall
+	    #self.walls.append(Wall())
+	  
+        shape = BulletConvexHullShape()
+
+        for p in self.hullpoints:
+	    shape.addPoint(p)
+
+        self.bnode = BulletRigidBodyNode()
+        self.bnode.addShape(shape)
+        self.np = utilities.app.render.attachNewNode(self.bnode)
+        self.np.setPos(pos.x,20,pos.y)
+
+
+
+class Pix():
+    def __init__(self, x, y):
+        self.x = int(x)
+        self.y = int(y)
+    def __repr__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+    
